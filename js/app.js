76fa7c73eac4f8ff7f37965a4325dd0db1ed8fe0
Seu Nome <fernandonetosoares@hotmail.com>
2024-08-15 13:31:37 -0300

    finalizado logica

--- js/app.js
+++ js/app.js
@@ -3,11 +3,39 @@ let amigos = [];
 function adicionar(){
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-    lista.textContent = ` ${lista.textContent} ${amigo.value},  `;
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
     amigos.push(amigo.value);
     amigo.value = '';
+
 }
 
 function sortear(){
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio')
+    for (let i = 0; i < amigos.length - 1; i++) {
+        sorteio.innerHTML += amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+    }
+    // Para fechar o ciclo
+    sorteio.innerHTML += amigos[amigos.length - 1] + ' --> ' + amigos[0] + '<br>';
+}
 
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar(){
+    amigos = [];
+    document.getElementById('lista-amigos').textContent = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
