c6cf87d6d98fb16c0d97d8e47c091d249fec153f
Seu Nome <fernandonetosoares@hotmail.com>
2024-08-15 17:35:55 -0300

    finalizado logica

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@ let amigos = [];
 
 function adicionar(){
     let amigo = document.getElementById('nome-amigo');
+
+    if (amigo.value == '') {
+        alert('Informe o nome do amigo')
+        return;
+    }
+
+    if (amigos.includes(amigo.value)) {
+        alert('Amigo já adicionado');
+        return;
+    }
+    
     let lista = document.getElementById('lista-amigos');
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
@@ -14,6 +25,12 @@ function adicionar(){
 }
 
 function sortear(){
+
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos')
+        return;
+    }
+
     embaralha(amigos);
     let sorteio = document.getElementById('lista-sorteio')
     for (let i = 0; i < amigos.length - 1; i++) {
